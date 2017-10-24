ad9ef06a7293255a5cadd10bbae64a57c4932ceb
Alexander Olofsson <alexander.olofsson@liu.se>
2017-10-24 13:14:06 +0200

    Strip user information automatically

--- client/UserCreation.vue
+++ client/UserCreation.vue
@@ -3,11 +3,11 @@
     <h6 class="pb-2">Create user:</h6>
 
     <form >
-      <input type="text" class="mt-2 form-control" placeholder="User's Full Name" aria-label="Full name" v-model="fullname"/>
-      <input type="email" class="mt-2 form-control" placeholder="email@example.com" aria-label="Email" v-model="email"/>
+      <input type="text" class="mt-2 form-control" placeholder="Full Name" aria-label="Full name" v-model.trim="fullname"/>
+      <input type="email" class="mt-2 form-control" placeholder="email@example.com" aria-label="Email" v-model.trim="email"/>
       <div class="mt-2 input-group">
         <span class="input-group-addon" id="username-at">@</span>
-        <input type="text" class="form-control" v-bind:placeholder="username_placeholder" aria-label="Username" aria-describedby="username-at" v-model="username"/>
+        <input type="text" class="form-control" v-bind:placeholder="username_placeholder" aria-label="Username" aria-describedby="username-at" v-model.trim="username"/>
       </div>
     </form>
   </div>
